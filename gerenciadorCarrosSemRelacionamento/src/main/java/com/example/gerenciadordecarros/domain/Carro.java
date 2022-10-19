434146b37dd33c33066640296ca10920d0ecc99d
MigJes <111096442+MigJes@users.noreply.github.com>
2022-10-19 15:23:50 -0300

    Add files via upload

---
+++ gerenciadorCarrosSemRelacionamento/src/main/java/com/example/gerenciadordecarros/domain/Carro.java
@@ -0,0 +1,45 @@
+package com.example.gerenciadordecarros.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "carros")
+public class Carro implements Serializable {
+
+    private static final long serialVersionUID = 1848135320742265025L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String modelo;
+    @OneToOne
+    private Documento documento;
+    @OneToOne
+    private Chave chave;
+    @ManyToOne
+    private Fabricante fabricante;
+    @ManyToMany
+    private List<Acessorio> acessorios;
+
+    
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    
+}
